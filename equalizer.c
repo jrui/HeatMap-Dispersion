97f225a33cc081a5eccdc763701b39a793daf921
João Miguel <jrsmiguel@outlook.pt>
2017-11-27 22:38:05 +0000

    Blur

--- equalizer.c
+++ equalizer.c
@@ -10,7 +10,7 @@ struct pixel {
 typedef struct pixel Pixel;
 
 struct image {
-  Pixel *pixels;
+  Pixel **pixels;
   int height, width, max, ident;
   char letter;
 };
@@ -20,17 +20,19 @@ typedef struct image Image;
 Image *read_image_file(char *file) {
   Image *img = (Image*) malloc(sizeof(struct image));
   FILE *fp;
-  int i = 0, j = 1;
+  int i, j;
 
   fp = fopen(file, "r");
   fscanf(fp, "%c%d %d %d %d\n", &img->letter, &img->ident, &img->width, &img->height, &img->max);
 
-  img->pixels = (Pixel*) malloc(sizeof(struct pixel) * img->width * img->height);
-  while(j > 0) {
-    j = fscanf(fp, "%d", &(&img->pixels[i])->r);
-    j = j > 0 ? fscanf(fp, "%d", &(&img->pixels[i])->g) : 0;
-    j = j > 0 ? fscanf(fp, "%d", &(&img->pixels[i])->b) : 0;
-    i++;
+  img->pixels = (Pixel**) malloc(sizeof(struct pixel) * img->width);
+  for(i = 0; i < img->width; i++) {
+    img->pixels[i] = (Pixel*) malloc(sizeof(struct pixel) * img->height);
+    for(j = 0; j < img->height; j++) {
+      fscanf(fp, "%d", &(&img->pixels[i][j])->r);
+      fscanf(fp, "%d", &(&img->pixels[i][j])->g);
+      fscanf(fp, "%d", &(&img->pixels[i][j])->b);
+    }
   }
   fclose(fp);
 
@@ -45,71 +47,73 @@ void write_image(char *file, Image *img) {
   FILE *fp2 = fopen(file, "w");
 
   fprintf(fp2, "%c%d %d %d %d\n", img->letter, img->ident, img->width, img->height, img->max);
-  j = (img->width * img->height);
-  for(i = 0; i < j; i++)
-    fprintf(fp2, "%d %d %d\n", img->pixels[i].r, img->pixels[i].g, img->pixels[i].b);
-
+  for(i = 0; i < img->width; i++)
+    for(j = 0; j < img->height; j++)
+      fprintf(fp2, "%d %d %d\n", img->pixels[i][j].r, img->pixels[i][j].g, img->pixels[i][j].b);
 }
 
 
 Image *heatmap(Image *img, int n_iter) {
-  Pixel *pixels = img->pixels;
+  Pixel **pixels = img->pixels;
   int cols = img->width, rows = img->height;
   int i, j, k;
-  printf("%d", &img->pixels[0].r);
 
   for(k = 0; k < n_iter; k++) {
     for(i = 0; i < cols; i++) {
       for(j = 0; j < rows; j++) {
         if(i == 0) {
           if(j == 0) {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i+1)*j].r + pixels[i*(j+1)].r) / 3);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i+1)*j].g + pixels[i*(j+1)].g) / 3);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i+1)*j].b + pixels[i*(j+1)].b) / 3);
-          }
-          else if(j == rows - 1) {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i-1)*j].r + pixels[i*(j+1)].r) / 3);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i-1)*j].g + pixels[i*(j+1)].g) / 3);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i-1)*j].b + pixels[i*(j+1)].b) / 3);
+            pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i+1][j].r + pixels[i][j+1].r) / 3);
+            pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i+1][j].g + pixels[i][j+1].g) / 3);
+            pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i+1][j].b + pixels[i][j+1].b) / 3);
           }
           else {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i-1)*j].r + pixels[(i+1)*j].r + pixels[i*(j+1)].r) / 4);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i-1)*j].g + pixels[(i+1)*j].g + pixels[i*(j+1)].g) / 4);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i-1)*j].b + pixels[(i+1)*j].b + pixels[i*(j+1)].b) / 4);
-          }
-        }
-        else if(i == cols - 1) {
-          if(j == 0) {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i+1)*j].r + pixels[i*(j-1)].r) / 3);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i+1)*j].g + pixels[i*(j-1)].g) / 3);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i+1)*j].b + pixels[i*(j-1)].b) / 3);
-          }
-          else if(j == rows - 1) {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i-1)*j].r + pixels[i*(j-1)].r) / 3);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i-1)*j].g + pixels[i*(j-1)].g) / 3);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i-1)*j].b + pixels[i*(j-1)].b) / 3);
-          }
-          else {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i-1)*j].r + pixels[(i+1)*j].r + pixels[i*(j-1)].r) / 4);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i-1)*j].g + pixels[(i+1)*j].g + pixels[i*(j-1)].g) / 4);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i-1)*j].b + pixels[(i+1)*j].b + pixels[i*(j-1)].b) / 4);
+            if(j == rows - 1) {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i+1][j].r + pixels[i][j-1].r) / 3);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i+1][j].g + pixels[i][j-1].g) / 3);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i+1][j].b + pixels[i][j-1].b) / 3);
+            }
+            else {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i+1][j].r + pixels[i][j-1].r + pixels[i][j+1].r) / 4);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i+1][j].g + pixels[i][j-1].g + pixels[i][j+1].g) / 4);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i+1][j].b + pixels[i][j-1].b + pixels[i][j+1].b) / 4);
+            }
           }
         }
         else {
-          if(j == 0) {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i+1)*j].r + pixels[i*(j+1)].r + pixels[i*(j-1)].r) / 4);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i+1)*j].g + pixels[i*(j+1)].g + pixels[i*(j-1)].g) / 4);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i+1)*j].b + pixels[i*(j+1)].b + pixels[i*(j-1)].b) / 4);
-          }
-          else if(j == rows - 1) {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i-1)*j].r + pixels[i*(j+1)].r + pixels[i*(j-1)].r) / 4);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i-1)*j].g + pixels[i*(j+1)].g + pixels[i*(j-1)].g) / 4);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i-1)*j].b + pixels[i*(j+1)].b + pixels[i*(j-1)].b) / 4);
+          if(i == cols - 1) {
+            if(j == 0) {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i-1][j].r + pixels[i][j+1].r) / 3);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i-1][j].g + pixels[i][j+1].g) / 3);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i-1][j].b + pixels[i][j+1].b) / 3);
+            }
+            else if(j == rows - 1) {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i-1][j].r + pixels[i][j-1].r) / 3);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i-1][j].g + pixels[i][j-1].g) / 3);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i-1][j].b + pixels[i][j-1].b) / 3);
+            }
+            else {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i-1][j].r + pixels[i][j+1].r + pixels[i][j-1].r) / 4);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i-1][j].g + pixels[i][j+1].g + pixels[i][j-1].g) / 4);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i-1][j].b + pixels[i][j+1].b + pixels[i][j-1].b) / 4);
+            }
           }
           else {
-            pixels[i*j].r = (int) floor((pixels[i*j].r + pixels[(i-1)*j].r + pixels[(i+1)*j].r + pixels[i*(j+1)].r + pixels[i*(j-1)].r) / 5);
-            pixels[i*j].g = (int) floor((pixels[i*j].g + pixels[(i-1)*j].g + pixels[(i+1)*j].g + pixels[i*(j+1)].g + pixels[i*(j-1)].g) / 5);
-            pixels[i*j].b = (int) floor((pixels[i*j].b + pixels[(i-1)*j].b + pixels[(i+1)*j].b + pixels[i*(j+1)].b + pixels[i*(j-1)].b) / 5);
+            if(j == 0) {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i+1][j].r + pixels[i-1][j].r + pixels[i][j+1].r) / 4);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i+1][j].g + pixels[i-1][j].g + pixels[i][j+1].g) / 4);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i+1][j].b + pixels[i-1][j].b + pixels[i][j+1].b) / 4);
+            }
+            else if(j == rows - 1) {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i-1][j].r + pixels[i+1][j].r + pixels[i][j-1].r) / 4);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i-1][j].g + pixels[i+1][j].g + pixels[i][j-1].g) / 4);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i-1][j].b + pixels[i+1][j].b + pixels[i][j-1].b) / 4);
+            }
+            else {
+              pixels[i][j].r = (int) floor((pixels[i][j].r + pixels[i-1][j].r + pixels[i+1][j].r + pixels[i][j+1].r + pixels[i][j-1].r) / 5);
+              pixels[i][j].g = (int) floor((pixels[i][j].g + pixels[i-1][j].g + pixels[i+1][j].g + pixels[i][j+1].g + pixels[i][j-1].g) / 5);
+              pixels[i][j].b = (int) floor((pixels[i][j].b + pixels[i-1][j].b + pixels[i+1][j].b + pixels[i][j+1].b + pixels[i][j-1].b) / 5);
+            }
           }
         }
       }
